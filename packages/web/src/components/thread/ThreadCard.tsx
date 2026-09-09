674b0324b4dfc90a0f20dd0ebc25aff9d68fe481
koizumib <bakuk0801@gmail.com>
2026-09-09 23:17:21 +0900

    Changes to be committed: 	modified:   packages/web/src/components/thread/ThreadCard.tsx 	modified:   packages/web/src/pages/MobileBoardPage.tsx
     Untracked files:
    	packages/web/src/components/mobile/SwipeHintOverlay.tsx

--- packages/web/src/components/thread/ThreadCard.tsx
+++ packages/web/src/components/thread/ThreadCard.tsx
@@ -47,7 +47,7 @@ export default function ThreadCard({ thread, isActive, isSelected, onClick, comp
   return (
     <div
       onClick={onClick}
-      className={`${compact ? 'p-2' : 'p-3'} border-b border-c-border cursor-pointer transition-colors relative ${
+      className={`${compact ? 'px-3 py-2' : 'px-4 py-3'} border-b border-c-border cursor-pointer transition-colors relative ${
         isSelected
           ? 'bg-c-accent/15'
           : isActive
--- packages/web/src/pages/MobileBoardPage.tsx
+++ packages/web/src/pages/MobileBoardPage.tsx
@@ -639,7 +639,7 @@ function MobileThreadViewInner({
           onTouchStart={handleViewTouchStart}
           onTouchMove={handleViewTouchMove}
           onTouchEnd={handleViewTouchEnd}
-          className="flex-1 overflow-y-auto custom-scrollbar py-1 space-y-2 [&::-webkit-scrollbar]:w-[4px]"
+          className="flex-1 overflow-y-auto custom-scrollbar px-3 py-1 space-y-2 [&::-webkit-scrollbar]:w-[4px]"
           style={{ overscrollBehaviorY: 'contain' }}
         >
         {/* 上プルインジケーター（スクロール内に配置してコンテンツを押し下げる） */}
